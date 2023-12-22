62e20e55721fedf401d0648f21298b6f9fbb6eca
NurAfifRaihan <114470342+NurAfifRaihan@users.noreply.github.com>
2023-12-22 23:33:57 +0700

    frontend

---
+++ resources/views/admin/layout/index.blade.php
@@ -0,0 +1,10 @@
+@include('admin.layout.header')
+<div id="layoutSidenav_content">
+    <main>
+        <div class="container-fluid px-4">
+            @yield('content')
+        </div>
+    </main>
+</div>
+</div>
+@include('admin.layout.footer')
